dc41764d39fffb40a9f6e05d195767734a654d93
aryan <bhatiaaryan587@gmail.com>
2026-04-08 12:52:51 +0530

    change

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -3,6 +3,7 @@ import AppRoute from "./Routes/AppRoute";
 import { UserProvider } from "./Context/user.context";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import {Analytics } from "@vercel/analytics"
 
 const App = () => (
   <UserProvider>
@@ -17,6 +18,7 @@ const App = () => (
       draggable
       theme="dark"
     />
+    <Analytics/>
   </UserProvider>
 );
 
