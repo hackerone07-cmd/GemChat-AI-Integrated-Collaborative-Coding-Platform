373e0a16dc36f2bd7e94cf1f42a5189e075ab486
aryan <bhatiaaryan587@gmail.com>
2026-07-12 09:35:16 +0530

    changes5

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import AppRoute from "./Routes/AppRoute";
-import { UserProvider } from "./Context/user.context";
+import { UserProvider } from "./Context/user.context.jsx";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
